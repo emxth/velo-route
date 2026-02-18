a1626549737af8dfd9226800cfae21885259bbba
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2026-02-18 12:27:07 +0530

    implement update and delete route controller

--- server/src/controllers/routeController.js
+++ server/src/controllers/routeController.js
@@ -27,13 +27,36 @@ export class RouteController {
         }
     }
 
+    //read spesific route
     async getById(req, res) {
         try {
             const route = await service.getRouteByID(req.params.id);
             res.status(201).json(route);
         } catch (err) {
             logger.error(err.message);
-            res.status(500).json({ error: `Server Error ${err.message}` });
+            res.status(404).json({ error: `Server Error ${err.message}` });
+        }
+    }
+
+    //request for update Route
+    async update(req, res) {
+        try {
+            const route = await service.updateRoute(req.params.id, req.body);
+            res.status(201).json(route);
+        } catch (err) {
+            logger.error(err.message);
+            res.status(404).json({ error: err.message });
+        }
+    }
+
+    //request for delete Route
+    async delete(req, res) {
+        try {
+            const result = await service.deleteRoute(req.params.id);
+            res.status(201).json(result);
+        } catch (err) {
+            logger.error(err.message);
+            res.status(404).json({ error: err.message });
         }
     }
 
